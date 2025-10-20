2efe3b64816a90c60da47b7fe9ba511647205f45
Nagy-Roland <kisrobert612@gmail.com>
2025-10-20 22:07:41 +0300

    f

---
+++ src/main/java/ro/umfst/ooplaborprojekt/GameSpec.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ro.umfst.ooplaborprojekt;
+
+/**
+ *
+ * @author Laptop
+ */
+public class GameSpec {
+    
+    private String os;
+    private String processor;
+    private String graphics;
+    private String memory;
+    private String storage;
+    
+        public GameSpec(){}
+        public GameSpec(String os,String processor,String graphics,String memory,String storage){
+        this.os = os;
+        this.processor = processor;
+        this.graphics = graphics;
+        this.memory = memory;
+        this.storage = storage;
+        
+    }
+    public void setOs(String os){
+        this.os = os;     
+    }   
+    
+    public void setProcessor(String processor){
+        this.processor = processor;
+    }  
+    
+    public void setGraphics(String graphics){
+        this.graphics = graphics;
+    }
+    
+    public void setMemory(String memory){
+        this.memory = memory;
+    }
+      
+    public void setStorage(String storage){
+        this.storage = storage;
+    }
+    
+    public String getOs(){
+        return os;
+    }
+    
+    public String getprocessor(){
+        return processor;
+    }
+    
+    public String getGraphics(){
+        return graphics;        
+    }
+    
+    public String getMemory(){
+        return memory;
+    }   
+        
+    public String getStorage(){
+        return storage;
+    }
+}
