6f2a84c9bb2012622f18ef03a5e32b3ed6959a5c
Nagy-Roland <kisrobert612@gmail.com>
2025-10-26 22:05:50 +0200

    f

--- src/main/java/ro/umfst/ooplaborprojekt/Ooplaborprojekt.java
+++ src/main/java/ro/umfst/ooplaborprojekt/Ooplaborprojekt.java
@@ -14,26 +14,42 @@ import java.io.InputStream;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
+import java.io.File;                 
+import java.io.FileNotFoundException; 
+import java.util.Scanner;   
 /**
  *
  * @author Laptop
  */
 public class Ooplaborprojekt extends JFrame {
-    DatabaseHelper db = new DatabaseHelper();
+    
+    DatabaseHelper db;
     JFrame frameResults;
     JFrame framePrimary;
     JFrame frameIdResults;
     
     public static void main(String[] args) {
-        
+      
         Ooplaborprojekt app = new Ooplaborprojekt();
-            app.jsonRead();
+        
+            app.jsonRead();           
             app.primaryJframeInitialization();
             app.resultsJframeInitialization();  
-           
+            app.idBasedResultsJframeInitialization(); 
+            app.dataBasePcComponentsInitialization();
     }
     
-   
+    public void dataBasePcComponentsInitialization(){
+        
+        db = new DatabaseHelper();
+          db.connectComps("jdbc:sqlite:PcComponentsPerfScore.db");          
+          db.ceatePcComponentTable("CPU");
+          db.ceatePcComponentTable("GPU");
+          db.ceatePcComponentTable("RAM");
+          db.ceatePcComponentTable("STORAGE");
+          db.fileReadAndWrite("PcComponents.txt");
+    }
     
     public void primaryJframeInitialization(){
         
@@ -63,7 +79,8 @@ public class Ooplaborprojekt extends JFrame {
         JLabel storageLabel = new JLabel("Enter the amount of your free storage space:");
             JTextField storageField = new JTextField(20);   
                 JButton submitButton = new JButton("Submit");
-         
+                    JButton openIdBasedSearchButton = new JButton ("Already have a setup submitted, search it by ID");
+                    
         panel.add(label);  
         panel.add(gameList);
         panel.add(Box.createVerticalStrut(100)); 
@@ -83,20 +100,27 @@ public class Ooplaborprojekt extends JFrame {
         panel.add(storageField);
         panel.add(Box.createVerticalStrut(50));
         panel.add(submitButton);
-        
+        panel.add(openIdBasedSearchButton);
         framePrimary.add(panel, BorderLayout.NORTH);       
         framePrimary.setVisible(true); 
         
         submitButton.addActionListener(new ActionListener() {
             @Override
                 public void actionPerformed(ActionEvent e) {
-                      setInputUsersPc(osField,cpuField,gpuField,memoryField,storageField);
-                      db.connect();
-                      db.createTable();
-                      db.close();
-                      showResults();
+                    setInputUsersPc(osField,cpuField,gpuField,memoryField,storageField);
+                        db.connectUsers("jdbc:sqlite:UsersPcSetup");
+                        db.createUserTable();
+                        db.closeUsers();
+                        showResults();
                 }
         }); 
+        
+        openIdBasedSearchButton.addActionListener(new ActionListener() {
+            @Override
+                public void actionPerformed(ActionEvent e){
+                    frameIdResults.setVisible(true);
+                }
+        });
     }
      
     public void resultsJframeInitialization(){
@@ -117,7 +141,7 @@ public class Ooplaborprojekt extends JFrame {
         JPanel panel3 = new JPanel();
         
         frameResults.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frameResults.setSize(1000, 800);
+        frameResults.setSize(800, 800);
         
         panel3.setLayout(new BoxLayout(panel3, BoxLayout.Y_AXIS));
         
@@ -134,7 +158,8 @@ public class Ooplaborprojekt extends JFrame {
         panel3.add(searchButton);
         panel3.add(resultsArea);
         
-        frameIdResults.add(panel3);
+        frameIdResults.add(panel3,BorderLayout.NORTH);
+      
     }
     
     public void jsonRead(){
@@ -160,6 +185,8 @@ public class Ooplaborprojekt extends JFrame {
         userspc.setStorage(storageField.getText());
             
     }
+    
+   
     
     public void showResults(){
         frameResults.setVisible(true);
